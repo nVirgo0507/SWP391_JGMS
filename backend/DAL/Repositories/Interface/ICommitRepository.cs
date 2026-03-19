3d5beb01d5441b56cc8a2bac0c81e81c389bebc5
nhannv <n.nhan99business@gmail.com>
2026-03-19 10:54:54 +0700

    [FEAT] add lecturer functions
    - add view requirements, progress report, commit statistic for lecturer

--- backend/DAL/Repositories/Interface/ICommitRepository.cs
+++ backend/DAL/Repositories/Interface/ICommitRepository.cs
@@ -6,6 +6,7 @@ namespace DAL.Repositories.Interface
     {
         Task<List<Commit>> GetCommitsByUserIdAsync(int userId);
         Task<List<Commit>> GetCommitsByUserIdAndProjectIdAsync(int userId, int projectId);
+        Task<List<Commit>> GetCommitsByProjectIdAsync(int projectId);
         Task<Commit?> GetByIdAsync(int commitId);
         System.Threading.Tasks.Task AddAsync(Commit commit);
         System.Threading.Tasks.Task AddRangeAsync(IEnumerable<Commit> commits);
