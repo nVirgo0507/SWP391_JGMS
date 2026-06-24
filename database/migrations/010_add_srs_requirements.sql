cecf4f89a6d1a96e658556afac4b2240979a25bc
nhannv <n.nhan99business@gmail.com>
2026-06-24 09:14:11 +0700

    [FEAT] add new database migration

---
+++ database/migrations/010_add_srs_requirements.sql
@@ -0,0 +1,10 @@
+ALTER TABLE "srs_document"
+    ADD COLUMN "glossary" text,
+ADD COLUMN "user_interfaces" text,
+ADD COLUMN "hardware_interfaces" text,
+ADD COLUMN "software_interfaces" text,
+ADD COLUMN "communications_interfaces" text,
+ADD COLUMN "performance_requirements" text,
+ADD COLUMN "security_requirements" text,
+ADD COLUMN "safety_requirements" text,
+ADD COLUMN "software_system_attributes" text;
